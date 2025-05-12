7231ee3b4b5d26a1cd51e51ba3c3dcdf1885970e
JOÃO CARLOS FORMENTO <joao_c_formento@estudante.sesisenai.org.br>
2025-05-12 15:59:08 -0300

    Utilização da API para criar imagens na Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,17 @@
 import Home from "./pages/Home";
-import Login from "./pages/Login"
+import Login from "./pages/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
 function App() {
-  
-
-  
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Home/>} />
-        <Route path="/Login" element={<Login/>} />
+        <Route index element={<Home />} />
+        <Route path="/Login" element={<Login />} />
         <Route path="/Dashboard" element={<h1>Pagina Dashboard</h1>} />
       </Routes>
     </BrowserRouter>
   );
-    <>
-      <div className="flex flex-row">
-        {data &&
-          data.message.map((message, index) => {
-            return (
-              <img
-                key={index}
-                src={message}
-                alt={message}
-                className=" max-w-50"
-              />
-            );
-          })}
-      </div>
-    </>
-
 }
 
 export default App;
